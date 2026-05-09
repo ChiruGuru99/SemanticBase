378a9f7c588608543452547bb375e08a2163e943
ChiruGuru99 <nvbagadi2810@gmail.com>
2026-05-09 07:41:41 +0530

    dependency update and external properties loading update

--- src/main/java/com/semanticbase/SemanticBaseApplication.java
+++ src/main/java/com/semanticbase/SemanticBaseApplication.java
@@ -1,9 +1,12 @@
 package com.semanticbase;
 
+import com.semanticbase.cache.CacheProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CacheProperties.class)
 public class SemanticBaseApplication {
 
     public static void main(String[] args) {
